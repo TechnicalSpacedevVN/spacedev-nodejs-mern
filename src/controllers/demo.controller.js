98cd81805462854e1e6644e33672ce27e3b64f20
Dang Thuyen Vuong <dangthuyenvuong@gmail.com>
2023-08-07 17:30:05 +0700

    day-9-prepare

--- src/controllers/demo.controller.js
+++ src/controllers/demo.controller.js
@@ -1,9 +1,8 @@
 import Joi from "joi";
 import { Controller, Get, Validate } from "../core/decorator/controller";
-import { BaseController } from "./base.controller";
 
 @Controller("/demo")
-export class DemoController extends BaseController {
+export class DemoController {
   @Get("/get-demo")
   @Validate(Joi.object({ query: Joi.string().required() }))
   getDemo() {
